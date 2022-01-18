c2df9637e9cb59e9ddb4fdb847ddd7252ca78d01
= <xolara1234@gmail.com>
2022-01-18 22:55:55 +0100

    Changed settings

--- wegy/settings.py
+++ wegy/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fw3%y(tvrms#9prh6otlnzb78=5d2s%361==y7@ayav+y09p=^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wegty.herokuapp.com']
 
 
 # Application definition
